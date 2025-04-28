04c74d36a193b19ee9da484f0b38e4d6e3d13380
jozz <yosefd99@gmail.com>
2025-04-28 04:39:09 +0300

    Update rate_limiter_guard.dart

--- lib/core/guards/rate_limiter_guard.dart
+++ lib/core/guards/rate_limiter_guard.dart
@@ -2,7 +2,7 @@ import 'package:firefast/core/guards/operation_limiter_base.dart';
 import 'package:firefast/firefast_core.dart';
 import 'package:prf/prf.dart';
 
-class RateLimiterGuard implements OperationLimiterBase, OperationGuard {
+class RateLimiterGuard extends OperationGuard implements OperationLimiterBase {
   final PrfRateLimiter limiter;
 
   const RateLimiterGuard(this.limiter);
