875d3ea0c854da26567efa8e0c0e4917f4536f80
jozz <yosefd99@gmail.com>
2025-05-27 09:38:09 +0300

    Updated imports

--- lib/firefast_firestore.dart
+++ lib/firefast_firestore.dart
@@ -1,12 +1,3 @@
 library;
 
-export 'firebase/firestore/adapters/adapters.dart';
-
-export 'firebase/firestore/extensions/fire_values.dart';
-export 'firebase/firestore/extensions/path.dart';
-export 'firebase/firestore/extensions/path_segment.dart';
-
-export 'firebase/firestore/models/datasource.dart';
-export 'firebase/firestore/models/output.dart';
-export 'firebase/firestore/models/path.dart';
-export 'firebase/firestore/models/firestore_document.dart';
+export 'src/firestore.dart';
